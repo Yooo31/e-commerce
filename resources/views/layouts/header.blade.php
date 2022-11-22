2d4ccfc746a9fb693bc0887acfbc46f4583713af
Yooo31 <mrts.yoan@gmail.com>
2022-11-22 23:00:31 +0100

    Add cart page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -46,7 +46,7 @@
                 </a>
               </li>
 							<li class="nav-item">
-                <a class="nav-link" href="contact">
+                <a class="nav-link" href="{{ route('cart.index') }}">
                 <i class="fas fa-shopping-cart mr-1"></i>Panier
                 </a>
               </li>
